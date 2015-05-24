6f59dd1c617f5a446ec13543380bf85be56d8df8
Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
2015-05-24 20:21:35 -0300

    Alteração do comentário do arquivo.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 /**
  * UTFPR - Universidade Tecnológica Federal do Paraná
  * DAINF - Departamento Acadêmico de Informática
- * IF62C - Fundamentos de Programação 2
  * 
  * Template de projeto de programa Java usando Maven.
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
